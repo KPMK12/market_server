15d241e65cfdab4819f82bc90137f8e836d1667e
khndhkx123 <khndhkx123@gmail.com>
2020-06-30 19:18:14 +0900

    feature(#35)-Add Springsecurity and JWT

---
+++ web/src/main/java/com/kpmk/market/security/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.kpmk.market.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                .antMatchers("/member/**").hasRole("ADMIN")
+                .antMatchers("/**").permitAll();
+    }
+}
